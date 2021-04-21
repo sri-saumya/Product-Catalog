46833d9bc709b42017aa58dd2a6a862a2d827707
Saumya Srivastava <78971508+sri-saumya@users.noreply.github.com>
2021-04-21 02:19:28 -0700

    ProductCatalog

--- Taazaa_Assignment2/Taazaa_Assignment2/Program.cs
+++ Taazaa_Assignment2/Taazaa_Assignment2/Program.cs
@@ -6,117 +6,119 @@ namespace Taazaa_Assignment2
     {
         static void Main(string[] args)
         {
+           
+            UserInput.input();
 
-            ProductOperations po = new ProductOperations();
-            CategoryOperation co = new CategoryOperation();
-            
-
-            
-            while (true)
-            {
-
-                Console.WriteLine("Select : CATEGORY|| Product || Exit : ");
-                string choice = Console.ReadLine();
-                Console.WriteLine("");
-
-
-                if (choice.ToUpper() == "PRODUCT")
-
-                {
-                    
-                    Console.WriteLine("Select : 1.GET all products || 2.SEARCH product by ID|| 3.ADD product || 4.Exit || 5.Back");
-                    string Pchoice = Console.ReadLine();
-                    Console.WriteLine("");
-
-                    if (Pchoice == "1")
-                    {
-                        po.GetProduct();
-                        
-                    }
-                    else if (Pchoice == "2")
-                    {
-                        Console.WriteLine("Enter id : ");
-                        int id = Int32.Parse(Console.ReadLine());
-                        po.SearchById(id);
-                       
-                    }
-                    else if (Pchoice == "3")
-                    {
-                        Console.WriteLine("Enter name");
-                        var cn = Console.ReadLine();
-                        Console.WriteLine("Enter short code");
-                        var sc = Console.ReadLine();
-                        Console.WriteLine("Enter description");
-                        var d = Console.ReadLine();
-                        Console.WriteLine("Enter selling price");
-                        var s = Int32.Parse(Console.ReadLine());
-                        ProductOperations.AddProduct(cn, sc, d, s);
-                        
-                    }
-                    else if (Pchoice == "4")
-                    {
-                        Console.WriteLine("EXIT From Product");
-                        
-                    }
-                    else if (Pchoice == "5")
-                    {
-                        Console.WriteLine("Please try again");
-                       
-                    }
-
-                }
+            //ProductOperations po = new ProductOperations();
+            //CategoryOperation co = new CategoryOperation();
 
 
 
-                if (choice.ToUpper() == "CATEGORY")
+            //while (true)
+            //{
 
-                {
-                
-                    Console.WriteLine("Select : 1.GET all category || 2.SEARCH category || 3.ADD Category || 4.Delete category || 5.Exit || 6.Back ");
-                    string Pchoice = Console.ReadLine();
-                    Console.WriteLine("");
-
-                    if (Pchoice == "1")
-                    {
-                        CategoryOperation.ListOfAllCategories();
-                      
-
-                    }
-                    else if (Pchoice == "2")
-                    {
-                        CategoryOperation.SearchCategory();
-                       
-                    }
-                    else if (Pchoice == "3")
-                    {
-                        Console.WriteLine("Enter name");
-                        var cn = Console.ReadLine();
-                        Console.WriteLine("Enter short code");
-                        var sc = Console.ReadLine();
-                        Console.WriteLine("Enter description");
-                        var d = Console.ReadLine();
-                        CategoryOperation.AddCategory(cn, sc, d);
-                    }
-                    else if (Pchoice == "4")
-                    {
-                        CategoryOperation.DeleteCategory();
-                        Console.WriteLine("Category Deleted");
-                     
-                    }
-                    else if (Pchoice == "5")
-                    {
-                        Console.WriteLine("EXIT from Category");
-                        //goto Catalogagain;
-                    }
-                    else if (Pchoice == "6")
-                    {
-                        Console.WriteLine("Please try again");
-                        
-                    }
-                }
+            //    Console.WriteLine("Select : CATEGORY|| Product || Exit : ");
+            //    string choice = Console.ReadLine();
+            //    Console.WriteLine("");
 
 
-            }      
+            //    if (choice.ToUpper() == "PRODUCT")
+
+            //    {
+
+            //        Console.WriteLine("Select : 1.GET all products || 2.SEARCH product by ID|| 3.ADD product || 4.Exit || 5.Back");
+            //        string Pchoice = Console.ReadLine();
+            //        Console.WriteLine("");
+
+            //        if (Pchoice == "1")
+            //        {
+            //            po.GetProduct();
+
+            //        }
+            //        else if (Pchoice == "2")
+            //        {
+            //            Console.WriteLine("Enter id : ");
+            //            int id = Int32.Parse(Console.ReadLine());
+            //            po.SearchById(id);
+
+            //        }
+            //        else if (Pchoice == "3")
+            //        {
+            //            Console.WriteLine("Enter name");
+            //            var cn = Console.ReadLine();
+            //            Console.WriteLine("Enter short code");
+            //            var sc = Console.ReadLine();
+            //            Console.WriteLine("Enter description");
+            //            var d = Console.ReadLine();
+            //            Console.WriteLine("Enter selling price");
+            //            var s = Int32.Parse(Console.ReadLine());
+            //            ProductOperations.AddProduct(cn, sc, d, s);
+
+            //        }
+            //        else if (Pchoice == "4")
+            //        {
+            //            Console.WriteLine("EXIT From Product");
+
+            //        }
+            //        else if (Pchoice == "5")
+            //        {
+            //            Console.WriteLine("Please try again");
+
+            //        }
+
+            //    }
+
+
+
+            //    if (choice.ToUpper() == "CATEGORY")
+
+            //    {
+
+            //        Console.WriteLine("Select : 1.GET all category || 2.SEARCH category || 3.ADD Category || 4.Delete category || 5.Exit || 6.Back ");
+            //        string Pchoice = Console.ReadLine();
+            //        Console.WriteLine("");
+
+            //        if (Pchoice == "1")
+            //        {
+            //            CategoryOperation.ListOfAllCategories();
+
+
+            //        }
+            //        else if (Pchoice == "2")
+            //        {
+            //            CategoryOperation.SearchCategory();
+
+            //        }
+            //        else if (Pchoice == "3")
+            //        {
+            //            Console.WriteLine("Enter name");
+            //            var cn = Console.ReadLine();
+            //            Console.WriteLine("Enter short code");
+            //            var sc = Console.ReadLine();
+            //            Console.WriteLine("Enter description");
+            //            var d = Console.ReadLine();
+            //            CategoryOperation.AddCategory(cn, sc, d);
+            //        }
+            //        else if (Pchoice == "4")
+            //        {
+            //            CategoryOperation.DeleteCategory();
+            //            Console.WriteLine("Category Deleted");
+
+            //        }
+            //        else if (Pchoice == "5")
+            //        {
+            //            Console.WriteLine("EXIT from Category");
+            //            goto Catalogagain;
+            //        }
+            //        else if (Pchoice == "6")
+            //        {
+            //            Console.WriteLine("Please try again");
+
+            //        }
+            //    }
+
+
+            //}
         }
     }
 }
